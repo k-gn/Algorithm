d5383de4fd0a3fa7c62cea5fe55fc5dcb4d735ac
k-gn <rlarbska97@gmail.com>
2022-02-02 18:22:04 +0900

    no message

--- javaAlgo/src/ch_1_recursion/RecursionEx1.java
+++ javaAlgo/src/ch_1_recursion/RecursionEx1.java
@@ -3,6 +3,17 @@ package ch_1_recursion;
 public class RecursionEx1 {
     public static void main(String[] args) {
 
-        System.out.println("test");
+        int n = 4;
+
+        int result = func(n);
+        System.out.println(result);
+    }
+
+    public static int func(int n) {
+        if(n <= 0) { // base case
+            return 0;
+        }else {
+            return n + func(n - 1); // recursive case
+        }
     }
 }
