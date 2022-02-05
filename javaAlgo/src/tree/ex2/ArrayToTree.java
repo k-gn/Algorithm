cf77cbfffd3eac2560553d26102b088f83ccacb4
k-gn <rlarbska97@gmail.com>
2022-02-05 21:27:24 +0900

    no message

--- javaAlgo/src/tree/ex2/ArrayToTree.java
+++ javaAlgo/src/tree/ex2/ArrayToTree.java
@@ -1,5 +1,8 @@
 package tree.ex2;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 class Tree {
     class Node {
         int data;
@@ -17,6 +20,7 @@ class Tree {
         root = makeTreeR(a, 0, a.length - 1);
     }
 
+    // array to binaryTree
     public Node makeTreeR(int[] a, int start, int end) {
         if(start > end) return null;
         int mid = (start + end) / 2;
@@ -38,8 +42,59 @@ class Tree {
         }else {
             System.out.println("found!");
         }
-
     }
+
+    // 레벨별 리스트 변형
+    ArrayList<LinkedList<Node>> BSTtoList() {
+        ArrayList<LinkedList<Node>> lists = new ArrayList<LinkedList<Node>>();
+        BSTtoList(root, lists, 0);
+        return lists;
+    }
+
+    void BSTtoList(Node root, ArrayList<LinkedList<Node>> lists, int level) {
+        if(root == null) return;
+        LinkedList<Node> list = null;
+        if(lists.size() == level) {
+            list = new LinkedList<>();
+            lists.add(list);
+        }else {
+            list = lists.get(level);
+        }
+
+        list.add(root);
+        BSTtoList(root.left, lists, level + 1);
+        BSTtoList(root.right, lists, level + 1);
+    }
+
+    ArrayList<LinkedList<Node>> BSTtoList2() {
+        ArrayList<LinkedList<Node>> result = new ArrayList<LinkedList<Node>>();
+        LinkedList<Node> current = new LinkedList<>();
+
+        if(root != null) {
+            current.add(root);
+        }
+
+        while(current.size() > 0) {
+            result.add(current);
+            LinkedList<Node> parents = current;
+            current = new LinkedList<>();
+            for(Node parent : parents) {
+                if(parent.left != null) current.add(parent.left);
+                if(parent.right != null) current.add(parent.right);
+            }
+        }
+        return result;
+    }
+
+    void print(ArrayList<LinkedList<Node>> arr) {
+        for(LinkedList<Node> list : arr) {
+            for(Node node : list) {
+                System.out.print(node.data + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
 public class ArrayToTree {
 
@@ -53,5 +108,7 @@ public class ArrayToTree {
         t.makeTree(a);
 
         t.searchBTree(t.root, 2);
+
+        t.print(t.BSTtoList());
     }
 }
