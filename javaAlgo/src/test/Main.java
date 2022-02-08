79db241d3de58877ddf2ee151b27e1ff5f35aac9
k-gn <rlarbska97@gmail.com>
2022-02-08 22:44:39 +0900

    no message

--- javaAlgo/src/test/Main.java
+++ javaAlgo/src/test/Main.java
@@ -7,10 +7,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void solution(int n) {
+    public static void solution(int[] arr, int n) {
 
-        
+        int[] rank = new int[n];
 
+        for(int i=0; i<rank.length; i++) {
+            rank[i] = 1;
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr.length; j++) {
+                if(arr[i] < arr[j]) {
+                    rank[i]++;
+                }
+            }
+        }
+
+        for(int i=0; i< rank.length; i++) {
+            System.out.print(rank[i] + " ");
+        }
     }
 
     public static void main(String[] args) {
@@ -18,7 +33,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = scanner.nextInt();
+        }
 
-        solution(n);
+        solution(arr, n);
     }
 }
