c2af5ca947d93a02475325c3a48bd46681b0b054
k-gn <rlarbska97@gmail.com>
2023-05-21 20:09:58 +0900

    feat: add array quiz

--- javaAlgo/src/haru/quiz/array/구간합_구하기.java
+++ javaAlgo/src/haru/quiz/array/구간합_구하기.java
@@ -1,26 +1,36 @@
 package haru.quiz.array;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /*
 	백준 온라인 저지 11659번
  */
 public class 구간합_구하기 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int max = 0;
-		int sum = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int suNo = Integer.parseInt(st.nextToken());
+		int quizNo = Integer.parseInt(st.nextToken());
+		long[] sumNumbers = new long[suNo + 1];
 
-		for (int i = 0; i < n; i++) {
-			int num = sc.nextInt();
-			sum += num;
-
-			if (num > max)
-				max = num;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= suNo; i++) {
+			sumNumbers[i] = sumNumbers[i - 1] + Integer.parseInt(st.nextToken());
 		}
 
-		System.out.println(sum * 100.0 / max / n);
+		System.out.println("sumNumbers = " + Arrays.toString(sumNumbers));
+
+		for (int q = 0; q < quizNo; q++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			System.out.println(sumNumbers[j] - sumNumbers[i - 1]);
+		}
 	}
 }
